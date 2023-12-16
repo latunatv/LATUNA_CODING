27d4b640c29b5331762e85abea1c23899693c225
Latuna <153551191+latunatv@users.noreply.github.com>
2023-12-16 18:59:22 +0100

    Update simplecalculator.py

--- python/simplecalculator.py
+++ python/simplecalculator.py
@@ -4,45 +4,47 @@
 # |_________________________________________________________________________|
 
 
-input("Warning! This program is a fun project and has not been bug-checked.")
-input("By continuing I am aware of the mistakes! (Press ENTER to continue)")
+print("__________________________________________________________________")
+print("__________________________________________________________________")
+print("Warning! This program is a fun project and has not been bug-checked.")
+print("By continuing I am aware of the mistakes! (Press ENTER to continue)")
+print("__________________________________________________________________")
+print("__________________________________________________________________")
+print("|Rechenarten: Addition, Subtraktion, Division, Multiplikation|")
 
-is_addition = input("Addition: True oder False: ")
+rechenart = input("Gib die Rechenart an: ")
 
-if is_addition == "True":
+if rechenart == "Addition":
     summand_1 = input("Gib den Wert des 1. Summanden an: ")
     summand_2 = input("Gib den Wert des 2. Summanden an: ")
-    ergebnis =float(summand_1) + float(summand_2)
+    ergebnis = float(summand_1) + float(summand_2)
+    print("Das Ergebnis ist " + str(ergebnis) + ".")
+
+
+if rechenart == "Subtraktion":
+    minuent = input("Gib den Wert des Minuenden an: ")
+    subtrahent = input("Gib den Wert des Subtrahenden an: ")
+    ergebnis = float(minuent) - float(subtrahent)
+    print("Das Ergebnis ist " + str(ergebnis) + ".")
+
+
+if rechenart == "Division":
+    divident = input("Gib den Wert des Dividenden an: ")
+    divisor = input("Gib den Wert des Divisoren an: ")
+    ergebnis = float(divident) / float(divisor)
+    print("Das Ergebnis ist " + str(ergebnis) + ".")
+
+
+if rechenart == "Multiplickation":
+    factor_1 = input("Gib den Wert des 1. Faktors an: ")
+    factor_2 = input("Gib den Wert des 2. Faktors an: ")
+    ergebnis = float(factor_1) * float(factor_2)
     print("Das Ergebnis ist " + str(ergebnis) + ".")
 
 else:
-    is_subtraction = input("Subtraktion: True oder False: ")
-    if is_subtraction == "True":
-        minuent = input("Gib den Wert des Minuenden an: ")
-        subtrahent = input("Gib den Wert des Subtrahenden an: ")
-        ergebnis = float(minuent) - float(subtrahent)
-        print("Das Ergebnis ist " + str(ergebnis) + ".")
-
-    else:
-        is_division = input("Division: True oder False: ")
-        if is_division == "True":
-            divident = input("Gib den Wert des Dividenden an: ")
-            divisor = input("Gib den Wert des Divisoren an: ")
-            ergebnis = float(divident) / float(divisor)
-            print("Das Ergebnis ist " + str(ergebnis) + ".")
-
-        else:
-            is_multiplication = input("Multiplikation: True oder False: ")
-            if is_multiplication == "True":
-                factor_1 = input("Gib den Wert des 1. Faktors an: ")
-                factor_2 = input("Gib den Wert des 2. Faktors an: ")
-                ergebnis = float(factor_1) * float(factor_2)
-                print("Das Ergebnis ist " + str(ergebnis) + ".")
-
-            else:
-                print("Es gibt keine weiteren Rechnungsmöglichkeiten!")
-                print(" Starte das Programm neu, wenn du erneut rechnen willst!")              
-input("Beliebige Taste drücken...")
+    print("Diese Rechenart ist (noch) nicht vorhanden!")
+    print(" Starte das Programm neu, wenn du erneut rechnen willst!")
+    input("Beliebige Taste drücken...")
 # |_________________________________________________________________________|
 # | This code was written by latunatv. GitHub: https://github.com/latunatv/ |
 # |              GitHub-Profile: https://github.com/latunatv/               |

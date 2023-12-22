84155588f1d4adff39b3b2a17aeea49d32b0d810
Latuna <153551191+latunatv@users.noreply.github.com>
2023-12-22 20:19:57 +0100

    Update simplecalculator.py

--- python/simplecalculator.py
+++ python/simplecalculator.py
@@ -25,32 +25,30 @@ if rechenart == "Addition":
     print("Das Ergebnis ist " + str(ergebnis) + ".")
 
 
-if rechenart == "Subtraktion":
+elif rechenart == "Subtraktion":
     minuend = input("Gib den Wert des Minuenden an: ")
     subtrahend = input("Gib den Wert des Subtrahenden an: ")
     ergebnis = float(minuend) - float(subtrahend)
     print("Das Ergebnis ist " + str(ergebnis) + ".")
 
-
-if rechenart == "Division":
+elif rechenart == "Division":
     dividend = input("Gib den Wert des Dividenden an: ")
-    divisor = input("Gib den Wert des Divisoren an: ")
+    divisor = input("Gib den Wert des Divisors an: ")
     ergebnis = float(dividend) / float(divisor)
     print("Das Ergebnis ist " + str(ergebnis) + ".")
 
-
-if rechenart == "Multiplikation":
+elif rechenart == "Multiplikation":
     factor_1 = input("Gib den Wert des 1. Faktors an: ")
     factor_2 = input("Gib den Wert des 2. Faktors an: ")
     ergebnis = float(factor_1) * float(factor_2)
     print("Das Ergebnis ist " + str(ergebnis) + ".")
 
-if rechenart == "Quadratwurzel":
+elif rechenart == "Quadratwurzel":
     zahl = float(input("Gib den Wert an, von dem die Quadratwurzel gezogen werden soll: "))
     wurzel = math.sqrt(zahl)
     print(f"Die Quadratwurzel von {zahl} ist {wurzel}.")
 
-if rechenart == "Potenz":
+elif rechenart == "Potenz":
     basis = input("Gib den Wert der Basis an: ")
     exponent = input("Gib den Wert der Potenz an: ")
     ergebnis = float(basis) ** float(exponent)
@@ -58,6 +56,7 @@ if rechenart == "Potenz":
 
 else:
     print("Diese Rechenart ist (noch) nicht vorhanden! / Fehler!")
+
 print(" Starte das Programm neu, wenn du erneut rechnen willst!")
 input("Beliebige Taste drücken...")
 

5e4cabbf7460c895f77f3611e3ffd9d1c9b3695f
Latuna <153551191+latunatv@users.noreply.github.com>
2024-01-22 15:58:38 +0100

    Create dice.py

---
+++ python/dice.py
@@ -0,0 +1,26 @@
+# |_________________________________________________________________________|
+# | This code was written by latunatv. GitHub-Repository: LATUNA_CODING     |
+# |              GitHub-Profile: https://github.com/latunatv/               |
+# |_________________________________________________________________________|
+
+# Import
+import random
+
+# Code
+input("To get a Number: press ENTER")
+numbers_dice = [1, 2, 3, 4, 5, 6]
+
+
+def g():
+    number = random.choice(numbers_dice)
+    print("Your number is " + str(number) + ".")
+
+
+while True:
+    g()
+    input()
+
+# |_________________________________________________________________________|
+# | This code was written by latunatv. GitHub-Repository: LATUNA_CODING     |
+# |              GitHub-Profile: https://github.com/latunatv/               |
+# |_________________________________________________________________________|
